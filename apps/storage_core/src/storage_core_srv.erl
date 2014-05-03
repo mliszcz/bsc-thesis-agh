b87282ed67126e6f5f02bce08b9dfe949275e602
mliszcz <liszcz.michal@gmail.com>
2014-05-03 14:33:53 +0200

    storage_core runner added

--- apps/storage_core/src/storage_core_srv.erl
+++ apps/storage_core/src/storage_core_srv.erl
@@ -6,43 +6,52 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/0, stop/0, echo/0]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+	gen_server:cast(?MODULE, stop).
+
+echo() ->
+	gen_server:call(?MODULE, none).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
 init(Args) ->
-    {ok, Args}.
+	{ok, Args}.
 
 handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+	io:format("server says hello!", []),
+	{reply, ok, State}.
+
+handle_cast(stop, State) ->
+	{stop, normal, State};
 
 handle_cast(_Msg, State) ->
-    {noreply, State}.
+	{noreply, State}.
 
 handle_info(_Info, State) ->
-    {noreply, State}.
+	{noreply, State}.
 
 terminate(_Reason, _State) ->
-    ok.
+	ok.
 
 code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+	{ok, State}.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
