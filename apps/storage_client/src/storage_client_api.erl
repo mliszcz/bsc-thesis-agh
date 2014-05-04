ef45840bc2caa62cf19e74d1c750da2554a3c5b9
mliszcz <liszcz.michal@gmail.com>
2014-05-04 22:25:21 +0200

    platform specific rebar

--- apps/storage_client/src/storage_client_api.erl
+++ apps/storage_client/src/storage_client_api.erl
@@ -2,10 +2,19 @@
 %% @doc Client API module for Storage Server
 
 -module(storage_client_api).
-%% -include("shared.hrl").
+-include("shared.hrl").
+-define(SERVER, storage_core_srv).
 % -define(HANDLERS, proc_handlers).
 
--export([echo/0]).
+-export([echo/0, request/0]).
 
 echo() ->
-	gen_server:call({storage_core_srv, 'ds@michal-pc'}, none, 1000).
+	gen_server:call({?SERVER, 'ds@michal-pc'}, none, 1000).
+
+request() ->
+	gen_server:call({?SERVER, 'ds@michal-pc'},
+		#request{	action	= write,
+					user_id	= michal,
+					v_path	= "path/to/file",
+					options	= none }
+				).
