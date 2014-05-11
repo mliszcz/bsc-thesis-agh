a1aa288f9624353d4e547706b9b6acddf013ee7e
mliszcz <liszcz.michal@gmail.com>
2014-05-11 12:08:53 +0200

    old startup scripts removed

--- run_http.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-COOKIE=ciacho
-NAME=http
-erl -setcookie $COOKIE -sname $NAME -pa apps/shared/ebin apps/storage_client/ebin apps/storage_http/ebin -s storage_http_srv start
--- run_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-COOKIE=ciacho
-NAME=ds
-
-erl -setcookie $COOKIE -sname $NAME -pa apps/shared/ebin apps/*/ebin -s storage_core_srv start_link
