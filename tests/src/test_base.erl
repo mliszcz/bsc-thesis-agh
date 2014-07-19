a0881f3ae32de767b1cf83c1d5f6f92cc840402b
mliszcz <liszcz.michal@gmail.com>
2014-07-19 19:38:40 +0100

    data creation test in test_base

--- tests/src/test_base.erl
+++ tests/src/test_base.erl
@@ -1,16 +1,43 @@
 
 -module(test_base).
+-define(VERBOSE, true).
 
--export([ run/2 ]).
+-export([ test_create/4 ]).
 
 % load_config() ->
 % 	pass.
 
-run(Threads, JobFun) ->
+%% ===================================================================
+%% general runner
+%% ===================================================================
+
+run(Threads, Iterations, JobFun) ->
 	SuperPid = self(),
-	lists:foreach(fun(_)-> spawn(fun() -> thread(SuperPid, JobFun) end) end, lists:seq(1, Threads)),
+	lists:foreach(fun(_)-> spawn(fun() -> thread(SuperPid, Iterations, JobFun) end) end, lists:seq(1, Threads)),
 	lists:map(fun(_)-> receive Res -> Res end end, lists:seq(1, Threads)).
 
-thread(Pid, JobFun) ->
-	{Microtime, _Value} = timer:tc(JobFun),
-	Pid ! Microtime.
+thread(Pid, Iterations, JobFun) ->
+	Pid ! iteration(Iterations, JobFun, 0).
+
+run(Threads, JobFun) ->
+	run(Threads, 1, fun(_) -> JobFun() end).
+
+iteration(0, _JobFun, TimeAcc) -> TimeAcc;
+iteration(N, JobFun,  TimeAcc) ->
+	if ?VERBOSE -> io:format("thread ~p iter ~p!~n", [self(), N]) end,
+	{Microtime, _Value} = timer:tc(JobFun, [N]),
+	iteration(N-1, JobFun, TimeAcc+Microtime).
+
+
+%% ===================================================================
+%% tests
+%% ===================================================================
+
+test_create(Node, Threads, Iterations, FilePath) ->
+	{ok, Binary} = file:read_file(FilePath),
+	RootPath = "performance/tests/file",
+	Result = run(Threads, Iterations, fun(N)->
+		{ok, created} = storage:create(Node, RootPath++integer_to_list(N), Binary)
+	end),
+
+	io:format("result is: ~p~n", [Result]).
