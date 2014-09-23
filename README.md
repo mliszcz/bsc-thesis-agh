8fdc45fdb90bbacd6c3420836bd547884cc647db
mliszcz <liszcz.michal@gmail.com>
2014-09-23 10:25:35 +0200

    readme updated

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ inz
 
 Praca Inzynierska
 
-### URGENT TODOS
-* make use of core gen_server state (stop using global vars)
-* handle constraint violations and other db errors
+### configuration
+* *setup_rhel.sh* script may be used to set everything up on red hat distros
+* the application uses sqlite3, so you have to compile driver on your own
 
 ### cluster deployment
 * change clustertool generator options in *clustertool/make_cluster.properties*
@@ -15,10 +15,9 @@ Praca Inzynierska
 ### third-party code
 * erlang-sqlite  
   https://github.com/alexeyr/erlang-sqlite3
-*
 
 ### REST API
-One can access files using HTTP requests and REST API. Supported methods:
+One can access files using HTTP requests and the REST API. Supported methods:
 * `POST` - create
 * `GET` - read
 * `PUT` - update
@@ -31,7 +30,7 @@ Example
 ```
 POST /storage/path/to/my/file.dat HTTP/1.1
 Host: ds-01.storage.example.com:9001
-Authorization: HMAC 0147d6e0a2d028d24438c90c27e6d149:6c51adc384572536d9c8a9dbcfbebf590942771f
+Authorization: HMAC user01:6c51adc384572536d9c8a9dbcfbebf590942771f
 Content-Type: application/octet-stream
 Content-Length: 2048
 <<binary data>>
@@ -39,30 +38,7 @@ Content-Length: 2048
 
 where:
 * *path/to/my/file.dat* is virtual file path
-* *0147d6e0a2d028d24438c90c27e6d149* is user id
+* *user01* is user id
 * *6c51adc384572536d9c8a9dbcfbebf590942771f* is *HMAC*  
   HMAC is calculated from request body (method, path, user id) and hashed using md5(*password*) as a key  
-  e.g.: `hmac_sha1(md5("secret"), "POSTpath/to/my/file.dat0147d6e0a2d028d24438c90c27e6d149") = 6c51adc384572536d9c8a9dbcfbebf590942771f`
-
-#### `/user` context
-
-**NOT SUPPORTED YET**
-
-Create user (derp:secret) request
-
-```
-POST /user/derp HTTP/1.1
-Host: ds-01.storage.example.com:9001
-Content-Type: text/plain; charset=UTF-8
-Content-Length: 6
-secret
-```
-
-In response we get new user id
-
-```
-HTTP/1.0 201 Created
-Content-Type: text/plain; charset=UTF-8
-Content-Length: 32
-0147d6e0a2d028d24438c90c27e6d149
-```
+  e.g.: `hmac_sha1(md5("secret"), "POSTpath/to/my/file.datuser01") = 6c51adc384572536d9c8a9dbcfbebf590942771f`
--- benchmark/benchmark_batch.sh
+++ benchmark/benchmark_batch.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
 
+for iter in 1 2 3 4 5 6 7 8 9 10
+do
+	for nodes in 1 2 3 4
+	do
+		./benchmark.sh ~ 	$nodes 			1 				_4K 			8000
+		./benchmark.sh ~ 	$nodes 			2 				_4K 			4000
+		./benchmark.sh ~ 	$nodes 			3 				_4K 			2660
+		./benchmark.sh ~ 	$nodes 			4 				_4K 			2000
+		./benchmark.sh ~ 	$nodes 			5 				_4K 			1600
+		./benchmark.sh ~ 	$nodes 			6 				_4K 			1330
+		./benchmark.sh ~ 	$nodes 			7 				_4K 			1140
+		./benchmark.sh ~ 	$nodes 			8 				_4K 			1000
+	done
+done
+
 #					NODES	|	THREADS		|	SIZE	|		ITER MAX
 
-./benchmark.sh ~ 	1 			1 				_4K 			800
-./benchmark.sh ~ 	1 			2 				_4K 			400
-./benchmark.sh ~ 	1 			3 				_4K 			266
-./benchmark.sh ~ 	1 			4 				_4K 			200
-./benchmark.sh ~ 	1 			5 				_4K 			160
-./benchmark.sh ~ 	1 			6 				_4K 			133
-./benchmark.sh ~ 	1 			7 				_4K 			114
-./benchmark.sh ~ 	1 			8 				_4K 			100
+# ./benchmark.sh ~ 	1 			1 				_4K 			8000
+# ./benchmark.sh ~ 	1 			2 				_4K 			4000
+# ./benchmark.sh ~ 	1 			3 				_4K 			2660
+# ./benchmark.sh ~ 	1 			4 				_4K 			2000
+# ./benchmark.sh ~ 	1 			5 				_4K 			1600
+# ./benchmark.sh ~ 	1 			6 				_4K 			1330
+# ./benchmark.sh ~ 	1 			7 				_4K 			1140
+# ./benchmark.sh ~ 	1 			8 				_4K 			1000
 
-./benchmark.sh ~ 	2 			1 				_4K 			800
-./benchmark.sh ~ 	2 			2 				_4K 			400
-./benchmark.sh ~ 	2 			3 				_4K 			266
-./benchmark.sh ~ 	2 			4 				_4K 			200
-./benchmark.sh ~ 	2 			5 				_4K 			160
-./benchmark.sh ~ 	2 			6 				_4K 			133
-./benchmark.sh ~ 	2 			7 				_4K 			114
-./benchmark.sh ~ 	2 			8 				_4K 			100
+# ./benchmark.sh ~ 	2 			1 				_4K 			8000
+# ./benchmark.sh ~ 	2 			2 				_4K 			4000
+# ./benchmark.sh ~ 	2 			3 				_4K 			2660
+# ./benchmark.sh ~ 	2 			4 				_4K 			2000
+# ./benchmark.sh ~ 	2 			5 				_4K 			1600
+# ./benchmark.sh ~ 	2 			6 				_4K 			1330
+# ./benchmark.sh ~ 	2 			7 				_4K 			1140
+# ./benchmark.sh ~ 	2 			8 				_4K 			1000
 
-./benchmark.sh ~ 	3 			1 				_4K 			800
-./benchmark.sh ~ 	3 			2 				_4K 			400
-./benchmark.sh ~ 	3 			3 				_4K 			266
-./benchmark.sh ~ 	3 			4 				_4K 			200
-./benchmark.sh ~ 	3 			5 				_4K 			160
-./benchmark.sh ~ 	3 			6 				_4K 			133
-./benchmark.sh ~ 	3 			7 				_4K 			114
-./benchmark.sh ~ 	3 			8 				_4K 			100
+# ./benchmark.sh ~ 	3 			1 				_4K 			8000
+# ./benchmark.sh ~ 	3 			2 				_4K 			4000
+# ./benchmark.sh ~ 	3 			3 				_4K 			2660
+# ./benchmark.sh ~ 	3 			4 				_4K 			2000
+# ./benchmark.sh ~ 	3 			5 				_4K 			1600
+# ./benchmark.sh ~ 	3 			6 				_4K 			1330
+# ./benchmark.sh ~ 	3 			7 				_4K 			1140
+# ./benchmark.sh ~ 	3 			8 				_4K 			1000
 
-./benchmark.sh ~ 	4 			1 				_4K 			800
-./benchmark.sh ~ 	4 			2 				_4K 			400
-./benchmark.sh ~ 	4 			3 				_4K 			266
-./benchmark.sh ~ 	4 			4 				_4K 			200
-./benchmark.sh ~ 	4 			5 				_4K 			160
-./benchmark.sh ~ 	4 			6 				_4K 			133
-./benchmark.sh ~ 	4 			7 				_4K 			114
-./benchmark.sh ~ 	4 			8 				_4K 			100
+# ./benchmark.sh ~ 	4 			1 				_4K 			8000
+# ./benchmark.sh ~ 	4 			2 				_4K 			4000
+# ./benchmark.sh ~ 	4 			3 				_4K 			2660
+# ./benchmark.sh ~ 	4 			4 				_4K 			2000
+# ./benchmark.sh ~ 	4 			5 				_4K 			1600
+# ./benchmark.sh ~ 	4 			6 				_4K 			1330
+# ./benchmark.sh ~ 	4 			7 				_4K 			1140
+# ./benchmark.sh ~ 	4 			8 				_4K 			1000
 
 # ./benchmark.sh ~ 	5 			1 				_4K 			8000
 # ./benchmark.sh ~ 	5 			2 				_4K 			4000
